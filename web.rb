b942fb2d4fbc4f3c9f5ab28ac0718ad58259834d
Ivan Sanchez <s4nchez@gmail.com>
2014-06-24 06:31:21 +0100

    Fixed browser detection

--- web.rb
+++ web.rb
@@ -4,7 +4,7 @@ require 'browser'
 get '/' do 'You probably wanted <a href="/go/destination-2">an example</a>. <br><br><br>Or perhaps you want to see the <a href="https://github.com/s4nchez/browser-specific-redirection">source code</a>?' end
 
 get '/go/:destination' do |destination|
-  browser = Browser.new(:ua => "request.env['HTTP_USER_AGENT']")
+  browser = Browser.new(:ua => "#{request.env['HTTP_USER_AGENT']}")
   platform = (browser.mobile? or browser.tablet?)? "mobile" : "desktop"
   redirect "/website/#{platform}##{destination}"
 end
